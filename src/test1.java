305bce50b4949313298d1f4eac12b44220948df5
Syncmaster_extreme <Syncmaster_extreme@Lenovo_e525>
2012-08-12 21:27:17 +0200

    added new file test1

---
+++ src/test1.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Syncmaster_extreme
+ *
+ */
+public class test1 {
+
+	/**
+	 * 
+	 */
+	public test1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Daniel Fay");
+		System.out.println("Wie gehts dir?");
+		// TODO Auto-generated method stube
+
+	}
+
+}
