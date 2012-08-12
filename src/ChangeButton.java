fb61a4be83c14d5c9d24a7131bf3c4bd677e21fc
Syncmaster_extreme <Syncmaster_extreme@Lenovo_e525>
2012-08-12 21:23:33 +0200

    first commit!

---
+++ src/ChangeButton.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+@SuppressWarnings("serial")
+public class ChangeButton  extends Applet {
+   int count  = 0; 
+   String buttonText   = "Number of Clicks: ";
+   Button b = new Button(buttonText + count);
+  
+    public void init () {
+      setBackground(Color.yellow);
+      add(b); 
+      b.setBackground(Color.blue);
+      b.setForeground(Color.yellow);
+      b.addActionListener(new ChangeButtonListener());
+    }
+
+class ChangeButtonListener implements ActionListener
+{
+    public void actionPerformed(ActionEvent e)
+    {   ++count;
+        b.setLabel(buttonText + count);
+       
+    }
+}
+}
