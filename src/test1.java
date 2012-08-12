34d3acf226a46341aff2caf19dc018fb1cc9bc7c
Syncmaster_extreme <Syncmaster_extreme@Lenovo_e525>
2012-08-12 21:30:16 +0200

    new line added

--- src/test1.java
+++ src/test1.java
@@ -21,6 +21,7 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Daniel Fay");
 		System.out.println("Wie gehts dir?");
+		System.out.println("Testing1");
 		// TODO Auto-generated method stube
 
 	}
