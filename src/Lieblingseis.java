c4f6ed4c7bd0590cdcffb745c090cf6b125616a9
Syncmaster_extreme <Syncmaster_extreme@Lenovo_e525>
2012-08-12 22:34:59 +0200

    add file Lieblingseis

---
+++ src/Lieblingseis.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+@SuppressWarnings("serial")
+public class Lieblingseis  extends Applet {
+
+    Label   l1 = new Label("Mein Lieblingseis:");
+    CheckboxGroup cg = new CheckboxGroup();
+    Checkbox c1 = new Checkbox("Heidelbeere",cg,  false), 
+             c2 = new Checkbox("Erdbeere",cg,  false),
+             c3 = new Checkbox("Vanille",cg, false),
+             c4 = new Checkbox("Schokolade",cg,  true);
+    TextField t = new TextField("Schokolade", 20);
+
+    public void init () {
+      setBackground(Color.pink);
+      
+      add(l1);
+      add(c1); 
+      c1.addItemListener(new EisListener());
+      add(c2);
+      c2.addItemListener(new EisListener());
+      add(c3);
+      c3.addItemListener(new EisListener());
+      add(c4);
+      c4.addItemListener(new EisListener());
+      add(t);
+    }
+
+class EisListener implements ItemListener
+{
+ public void itemStateChanged(ItemEvent ie)
+    {   
+     t.setText((String)ie.getItem());
+    }
+}
+
+}
